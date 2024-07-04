28e4eeaf38949ad01e27eef72ffaa886adf864dd
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-04 13:58:27 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.25 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = "";
+        
+        while (n != 0) {
+            str = str + (n % 3);
+            n = n / 3;            
+        }
+        
+        return answer = Integer.parseInt(str, 3);        
+    }
+}
