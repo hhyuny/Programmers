15fce6f7b76254f9908bf0c65cc9992cd98527ad
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-08 08:10:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,9 @@
+function solution(n) {   
+    // if (Math.sqrt(n) % 1 === 0) {
+    //     return (Math.sqrt(n) + 1) ** 2;
+    // } else {
+    //     return -1
+    // }
+    
+    return Math.sqrt(n) % 1 === 0 ? (Math.sqrt(n) + 1) ** 2 : -1;
+}
