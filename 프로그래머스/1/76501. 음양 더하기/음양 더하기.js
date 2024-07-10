1128d3a54be874b0daab7d7e557f983ae6755a56
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-10 10:24:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,17 +1,3 @@
-function solution(absolutes, signs) {
-    
-    let answer = 0;
-    
-    for (let i = 0; i < signs.length; i++) {
-        if (signs[i] === false) {
-            answer += -absolutes[i];
-        } else {
-            answer += absolutes[i];
-        }
-    }
-    
-    
-    
-    
-    return answer;
+function solution(absolutes, signs) {    
+    return absolutes.reduce((acc, cur, idx) => acc + (signs[idx] === true ? cur * 1 : cur * (-1)), 0);
 }
