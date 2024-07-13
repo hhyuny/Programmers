4e18a9d41c60e718d95ee4642d8ec203d8a2b349
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-13 09:38:01 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.38 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,13 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    
-    for (let i = 0; i < arr1.length; i++) {
-    	answer[i] = [];
-        for (let j = 0; j < arr1[0].length; j++) {
-            answer[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
-    return answer;
+    return arr1.map((item1, idx1) => item1.map((item2, idx2) => item2 + arr2[idx1][idx2]));
 }
+
+// ex. 첫 사이클
+// item1 = [1, 2]
+// idx1 = 0
+
+// 여기서 item1에 map을 쓴다.
+// item2 = 1
+// idx2 = 0
+
+// item2(=1) + arr2[0][0](=3) = 1 + 3 = 4
