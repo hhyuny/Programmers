523f776eaa289025d4d5bdb663344b0ec3ef9171
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-13 21:54:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,12 +1,3 @@
 function solution(n) {
-    let arr = [];
-    let count = 0;
-    
-    while (!(n === 0)) {
-        arr[count] = n % 3;
-        n = Math.floor(n / 3);
-        count++;
-    }
-    
-    return arr.reduce((acc, cur, idx) => acc + 3 ** (arr.length - 1 - idx) * cur, 0);
+    return parseInt(n.toString(3).split('').reverse().join(''), 3);
 }
