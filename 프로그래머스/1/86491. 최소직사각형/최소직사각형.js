35ac541d4ffacb7cc85bad80fed6341dd73df9be
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-17 20:38:10 +0900

    [level 1] Title: 최소직사각형, Time: 6.03 ms, Memory: 40.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,12 @@
+function solution(sizes) {
+    let newSizes = sizes.map(([w, h]) => w > h ? [w, h] : [h, w]);
+    
+    let walletSize = [0, 0];
+    
+    newSizes.forEach(([w, h]) => {
+        w > walletSize[0] ? walletSize[0] = w : walletSize[0];
+        h > walletSize[1] ? walletSize[1] = h : walletSize[1];
+    })
+    
+    return walletSize[0] * walletSize[1];    
+}
