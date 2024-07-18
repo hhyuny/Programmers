76fa2be47a6ff6baab367b9f5a34e9769c498be5
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-07-18 10:16:53 +0900

    [level 1] Title: 시저 암호, Time: 1.90 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,29 +1,25 @@
 function solution(s, n) {
     let arr = s.split("");
-    
+
     let newArr = arr.map((item) => {
-    
-    if (item !== " ") {
-        if (item.charCodeAt() >= 97) {
-            if (item.charCodeAt() + n > 122) {
-            item = String.fromCharCode(item.charCodeAt() + n - 26);
-        } else {
-            item = String.fromCharCode(item.charCodeAt() + n);
+        if (item !== " ") {
+            if (item.charCodeAt() >= 97) {
+                if (item.charCodeAt() + n > 122) {
+                    item = String.fromCharCode(item.charCodeAt() + n - 26);
+                } else {
+                    item = String.fromCharCode(item.charCodeAt() + n);
+                }
+            } else {
+                if (item.charCodeAt() + n > 90) {
+                    item = String.fromCharCode(item.charCodeAt() + n - 26);
+                } else {
+                    item = String.fromCharCode(item.charCodeAt() + n);
+                }
+            }
         }
-        } else {
-              if (item.charCodeAt() + n > 90) {
-            item = String.fromCharCode(item.charCodeAt() + n - 26);
-        } else {
-            item = String.fromCharCode(item.charCodeAt() + n);
-        }
-        }
-        
-        
-        
-    }
-    
-    return item;
-});
-    
-    return newArr.join('');
+
+        return item;
+    });
+
+    return newArr.join("");
 }
