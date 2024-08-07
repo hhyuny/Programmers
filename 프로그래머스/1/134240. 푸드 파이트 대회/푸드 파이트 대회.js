5dba474eecbe4082fe9b073f5288cac6e3ddf0b4
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-08-07 19:43:53 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.20 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,14 +2,9 @@ function solution(food) {
     let arr = [];
 
 for (let i = 1; i < food.length; i++) {
-  if (food[i] % 2 !== 0) {
-    for (let j = 0; j < (food[i] - 1) / 2; j++) {
-      arr.push(i);
-    }
-  } else {
-    for (let j = 0; j < food[i] / 2; j++) {
-      arr.push(i);
-    }
+  let count = food[i] % 2 === 0 ? food[i] / 2 : (food[i] - 1) / 2;
+  for (let j = 0; j < count; j++) {
+    arr.push(i);
   }
 }
     
