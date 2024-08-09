fa04c5251e7cf473424aaf763ec1ec12dd999dfe
Hyun <168260336+hhyuny@users.noreply.github.com>
2024-08-09 09:43:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 29.25 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,20 +1,20 @@
 function solution(k, score) {
-    let temp = [];
-let answer = [];
+  let temp = [];
+  let answer = [];
 
-for (let i = 0; i < score.length; i++) {
-  if (i < k) {
-    temp.push(score[i]);
-    temp.sort((a, b) => b - a);
-    console.log(temp);
-    answer[i] = temp[i];
-  } else {
-    temp.push(score[i]);
-    temp.sort((a, b) => b - a);
-    temp.pop();
-    answer[i] = temp[k - 1];
+  for (let i = 0; i < score.length; i++) {
+    if (i < k) {
+      temp.push(score[i]);
+      temp.sort((a, b) => b - a);
+      console.log(temp);
+      answer[i] = temp[i];
+    } else {
+      temp.push(score[i]);
+      temp.sort((a, b) => b - a);
+      temp.pop();
+      answer[i] = temp[k - 1];
+    }
   }
-}
-    
-    return answer;
+
+  return answer;
 }
